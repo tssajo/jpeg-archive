09df606d6ee47c9a1cb35a0b92a81b0f28cf4b4d
tssajo <tssajo@gmail.com>
2019-03-16 19:14:30 +0100

    Added some improvements for compilation on Windows

--- compile-on-windows.cmd
+++ compile-on-windows.cmd
@@ -1,5 +1,7 @@
 @ECHO OFF
 CD src\iqa
+REM You might need to find your vcvarsall.bat file on your Windows computer and then CALL that from here like this:
+REM CALL "C:\path\to\your\vcvarsall.bat" x64
 MsBuild.exe iqa.sln /t:Build /p:Configuration=Release /p:Platform=x64 /p:DebugSymbols=false /p:DebugType=None
 CD ..\..
 nmake /NOLOGO -f Makefile.w32
