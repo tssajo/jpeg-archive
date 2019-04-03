4b73478efc4f86af7a930e7e124d3d581335b1ac
tssajo <tssajo@gmail.com>
2019-04-03 15:38:09 +0200

    Various improvements in jpeg-recompress.c

--- jpeg-recompress.c
+++ jpeg-recompress.c
@@ -1,7 +1,7 @@
 /*
     Recompress a JPEG file while attempting to keep visual quality the same
     by using structural similarity (SSIM) as a metric. Does a binary search
-    between JPEG quality 40 and 95 to find the best match. Also makes sure
+    between JPEG quality 1 and 99 to find the best match. Also makes sure
     that huffman tables are optimized if they weren't already.
 */
 
@@ -11,6 +11,7 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
+#include <float.h>
 
 #include "src/edit.h"
 #include "src/iqa/include/iqa.h"
@@ -23,6 +24,7 @@
 #endif
 
 const char *COMMENT = "Compressed by jpeg-recompress";
+const int minDelta = 10;
 
 // Comparison method
 enum METHOD {
@@ -33,10 +35,18 @@ enum METHOD {
     MPE
 };
 
+const char methodName[5][9] = {
+    "",
+    "ssim",
+    "ms-ssim",
+    "smallfry",
+    "mpe"
+};
+
 int method = SSIM;
 
 // Number of binary search steps
-int attempts = 6;
+int attempts = 8;
 
 // Target quality (SSIM) value
 enum QUALITY_PRESET {
@@ -50,8 +60,8 @@ float target = 0;
 int preset = MEDIUM;
 
 // Min/max JPEG quality
-int jpegMin = 40;
-int jpegMax = 95;
+int qMin = 1;
+int qMax = 99;
 
 // Strip metadata from the file?
 int strip = 0;
@@ -119,64 +129,64 @@ static void setTargetFromPreset() {
         case SSIM:
             switch (preset) {
                 case LOW:
-                    target = 0.999;
+                    target = (float)0.999;
                     break;
                 case MEDIUM:
-                    target = 0.9999;
+                    target = (float)0.9999;
                     break;
                 case HIGH:
-                    target = 0.99995;
+                    target = (float)0.99995;
                     break;
                 case VERYHIGH:
-                    target = 0.99999;
+                    target = (float)0.99999;
                     break;
             }
             break;
         case MS_SSIM:
             switch (preset) {
                 case LOW:
-                    target = 0.85;
+                    target = (float)0.85;
                     break;
                 case MEDIUM:
-                    target = 0.94;
+                    target = (float)0.94;
                     break;
                 case HIGH:
-                    target = 0.96;
+                    target = (float)0.96;
                     break;
                 case VERYHIGH:
-                    target = 0.98;
+                    target = (float)0.98;
                     break;
             }
             break;
         case SMALLFRY:
             switch (preset) {
                 case LOW:
-                    target = 100.75;
+                    target = (float)100.75;
                     break;
                 case MEDIUM:
-                    target = 102.25;
+                    target = (float)102.25;
                     break;
                 case HIGH:
-                    target = 103.8;
+                    target = (float)103.8;
                     break;
                 case VERYHIGH:
-                    target = 105.5;
+                    target = (float)105.5;
                     break;
             }
             break;
         case MPE:
             switch (preset) {
                 case LOW:
-                    target = 1.5;
+                    target = (float)1.5;
                     break;
                 case MEDIUM:
-                    target = 1.0;
+                    target = (float)1.0;
                     break;
                 case HIGH:
-                    target = 0.8;
+                    target = (float)0.8;
                     break;
                 case VERYHIGH:
-                    target = 0.6;
+                    target = (float)0.6;
                     break;
             }
             break;
@@ -224,9 +234,9 @@ void usage(void) {
     printf("  -h, --help                   output program help\n");
     printf("  -t, --target [arg]           set target quality [0.9999]\n");
     printf("  -q, --quality [arg]          set a quality preset: low, medium, high, veryhigh [medium]\n");
-    printf("  -n, --min [arg]              minimum JPEG quality [40]\n");
-    printf("  -x, --max [arg]              maximum JPEG quality [95]\n");
-    printf("  -l, --loops [arg]            set the number of runs to attempt [6]\n");
+    printf("  -n, --min [arg]              minimum JPEG quality [1]\n");
+    printf("  -x, --max [arg]              maximum JPEG quality [99]\n");
+    printf("  -l, --loops [arg]            set the number of runs to attempt [8]\n");
     printf("  -a, --accurate               favor accuracy over speed\n");
     printf("  -m, --method [arg]           set comparison method to one of 'mpe', 'ssim', 'ms-ssim', 'smallfry' [ssim]\n");
     printf("  -s, --strip                  strip metadata\n");
@@ -238,6 +248,26 @@ void usage(void) {
     printf("  -S, --subsample [arg]        set subsampling method to one of 'default', 'disable' [default]\n");
     printf("  -T, --input-filetype [arg]   set input file type to one of 'auto', 'jpeg', 'ppm' [auto]\n");
     printf("  -Q, --quiet                  only print out errors\n");
+}
+
+int copyFile(char *outputPath, unsigned char *buf, long bufSize) {
+    FILE *file = openOutput(outputPath);
+    if (file == NULL) {
+        error("could not open output file: %s", outputPath);
+        return 1;
+    }
+    int wSize = fwrite(buf, bufSize, 1, file);
+    if (wSize != 1) {
+        fclose(file);
+        error("could not write to output file: %s", outputPath);
+        return 1;
+    }
+    int err = fclose(file);
+    if (err) {
+        error("could not close the output file: %s", outputPath);
+        return 1;
+    }
+    return 0;
 }
 
 int main (int argc, char **argv) {
@@ -282,10 +312,10 @@ int main (int argc, char **argv) {
             preset = parseQuality(optarg);
             break;
         case 'n':
-            jpegMin = atoi(optarg);
+            qMin = atoi(optarg);
             break;
         case 'x':
-            jpegMax = atoi(optarg);
+            qMax = atoi(optarg);
             break;
         case 'l':
             attempts = atoi(optarg);
@@ -341,6 +371,11 @@ int main (int argc, char **argv) {
         return 255;
     }
 
+    if (qMin > qMax) {
+        error("maximum JPEG quality must not be smaller than minimum JPEG quality!");
+        return 1;
+    }
+
     // No target passed, use preset!
     if (!target) {
         setTargetFromPreset();
@@ -354,18 +389,23 @@ int main (int argc, char **argv) {
     long originalGraySize = 0;
     unsigned char *compressed = NULL;
     unsigned long compressedSize = 0;
+    unsigned long totalSize = 0;
     unsigned char *compressedGray;
     long compressedGraySize = 0;
     unsigned char *tmpImage;
     int width, height;
-    unsigned char *metaBuf;
+    unsigned char *metaBuf = NULL;
     unsigned int metaSize = 0;
+    unsigned int metaSizeCOM = strlen(COMMENT) + 4;
     FILE *file;
     char *inputPath = argv[optind];
     char *outputPath = argv[optind + 1];
 
     /* Read the input into a buffer. */
     bufSize = readFile(inputPath, (void **) &buf);
+    if (!bufSize) {
+        return 1;
+    }
 
     /* Detect input file type. */
     if (inputFiletype == FILETYPE_AUTO)
@@ -378,6 +418,7 @@ int main (int argc, char **argv) {
     originalSize = decodeFileFromBuffer(buf, bufSize, &original, inputFiletype, &width, &height, JCS_RGB);
     if (!originalSize) {
         error("invalid input file: %s", inputPath);
+        free(buf);
         return 1;
     }
 
@@ -391,23 +432,25 @@ int main (int argc, char **argv) {
 
     // Convert RGB input into Y
     originalGraySize = grayscale(original, &originalGray, width, height);
+    if (!originalGraySize) {
+        free(original);
+        free(buf);
+        return 1;
+    }
 
     if (inputFiletype == FILETYPE_JPEG) {
         // Read metadata (EXIF / IPTC / XMP tags)
         if (getMetadata(buf, bufSize, &metaBuf, &metaSize, COMMENT)) {
+
+            free(originalGray);
+            free(original);
+
             if (copyFiles) {
                 info("File already processed by jpeg-recompress!\n");
-                file = openOutput(outputPath);
-                if (file == NULL) {
-                    error("could not open output file: %s", outputPath);
-                    return 1;
-                }
 
-                fwrite(buf, bufSize, 1, file);
-                fclose(file);
+                copyFile(outputPath, buf, bufSize);
 
                 free(buf);
-
                 return 0;
             } else {
                 error("file already processed by jpeg-recompress!");
@@ -420,90 +463,107 @@ int main (int argc, char **argv) {
     if (strip) {
         // Pretend we have no metadata
         metaSize = 0;
+        if (metaBuf != NULL) {
+            free(metaBuf);
+            metaBuf = NULL;
+        }
     } else {
         info("Metadata size is %ukb\n", metaSize / 1024);
     }
 
-    if (!originalSize || !originalGraySize) { return 1; }
-
-    if (jpegMin > jpegMax) {
-        error("maximum JPEG quality must not be smaller than minimum JPEG quality!");
-        return 1;
-    }
-
     // Do a binary search to find the optimal encoding quality for the
     // given target SSIM value.
-    int min = jpegMin, max = jpegMax;
+    float newDiff;
+    float bestDiff = FLT_MAX;
+    int bestQuality = INT_MIN;
+    int quality;
+    int min = qMin, max = qMax;
     for (int attempt = attempts - 1; attempt >= 0; --attempt) {
-        float metric;
-        int quality = min + (max - min) / 2;
+        quality = (min + max) / 2;
 
-        /* Terminate early once bisection interval is a singleton. */
+        // We were already at this quality level? If yes then let's make this
+        // the final run and the final image will be processed optimized this time
+        if (quality == bestQuality)
+            attempt = 0;
+
+        // Terminate early once bisection interval is a singleton.
         if (min == max)
             attempt = 0;
 
         int progressive = attempt ? 0 : !noProgressive;
         int optimize = accurate ? 1 : (attempt ? 0 : 1);
 
+        // If compressed buffer is not empty the free it
+        if (compressed != NULL) {
+            free(compressed);
+            compressed = NULL;
+        }
+
         // Recompress to a new quality level, without optimizations (for speed)
         compressedSize = encodeJpeg(&compressed, original, width, height, JCS_RGB, quality, progressive, optimize, subsample);
 
         // Load compressed luma for quality comparison
         compressedGraySize = decodeJpeg(compressed, compressedSize, &compressedGray, &width, &height, JCS_GRAYSCALE);
-
         if (!compressedGraySize) {
-          error("unable to decode file that was just encoded!");
-          return 1;
-        }
+            error("unable to decode file that was just encoded!");
 
-        if (!attempt) {
-            info("Final optimized ");
+            free(compressed);
+            if (metaBuf != NULL)
+                free(metaBuf);
+            free(originalGray);
+            free(original);
+            free(buf);
+
+            return 1;
         }
 
         // Measure quality difference
+        float metric;
         switch (method) {
             case MS_SSIM:
                 metric = iqa_ms_ssim(originalGray, compressedGray, width, height, width, 0);
-                info("ms-ssim");
                 break;
             case SMALLFRY:
                 metric = smallfry_metric(originalGray, compressedGray, width, height);
-                info("smallfry");
                 break;
             case MPE:
                 metric = meanPixelError(originalGray, compressedGray, width, height, 1);
-                info("mpe");
                 break;
             case SSIM: default:
                 metric = iqa_ssim(originalGray, compressedGray, width, height, width, 0, 0);
-                info("ssim");
                 break;
         }
 
-        if (attempt) {
-            info(" at q=%i (%i - %i): %f\n", quality, min, max, metric);
-        } else {
-            info(" at q=%i: %f\n", quality, metric);
+        // We no longer need compressedGray
+        free(compressedGray);
+
+        newDiff = fabs(target - metric);
+        if (newDiff < bestDiff) {
+            bestDiff = newDiff;
+            bestQuality = quality;
         }
 
+        if (attempt) {
+            info("%s at q=%i (%i - %i): %f (target is %f difference is %f)\n", methodName[method], quality, min, max, metric, target, newDiff);
+        } else {
+            info("Final optimized %s at q=%i (%i - %i): %f (target was %f, difference is %f)\n", methodName[method], quality, min, max, metric, target, newDiff);
+        }
+
+        totalSize = compressedSize + metaSizeCOM + metaSize;
         if (metric < target) {
-            if (compressedSize >= bufSize) {
+            if (totalSize + minDelta >= bufSize) {
                 free(compressed);
-                free(compressedGray);
+                if (metaBuf != NULL)
+                    free(metaBuf);
+                free(originalGray);
+                free(original);
 
                 if (copyFiles) {
                     info("Output file would be larger than input!\n");
-                    file = openOutput(outputPath);
-                    if (file == NULL) {
-                        error("could not open output file: %s", outputPath);
-                        return 1;
-                    }
 
-                    fwrite(buf, bufSize, 1, file);
-                    fclose(file);
+                    copyFile(outputPath, buf, bufSize);
 
                     free(buf);
-
                     return 0;
                 } else {
                     error("output file would be larger than input!");
@@ -535,74 +595,133 @@ int main (int argc, char **argv) {
             }
         }
 
-        // If we aren't done yet, then free the image data
-        if (attempt) {
-            free(compressed);
-            free(compressedGray);
-        }
+    }
+
+    free(originalGray);
+    free(original);
+
+    // Calculate and show savings, if any
+    int percent = totalSize * 100 / bufSize;
+    unsigned long saved = (bufSize > totalSize) ? bufSize - totalSize : 0;
+    info("New size is %i%% of original (saved %lu kb)\n", percent, saved / 1024);
+
+    if (totalSize >= bufSize) {
+        error("output file is larger than input, aborting!");
+
+        copyFile(outputPath, buf, bufSize);
+
+        free(compressed);
+        if (metaBuf != NULL)
+            free(metaBuf);
+        free(buf);
+
+        return 1;
     }
 
     free(buf);
 
-    // Calculate and show savings, if any
-    int percent = (compressedSize + metaSize) * 100 / bufSize;
-    unsigned long saved = (bufSize > compressedSize) ? bufSize - compressedSize - metaSize : 0;
-    info("New size is %i%% of original (saved %lu kb)\n", percent, saved / 1024);
-
-    if (compressedSize >= bufSize) {
-        error("output file is larger than input, aborting!");
-        return 1;
-    }
-
-    // Open output file for writing
-    file = openOutput(outputPath);
-    if (file == NULL) {
-        error("could not open output file");
-        return 1;
-    }
-
     /* Check that the metadata starts with a SOI marker. */
     if (!checkJpegMagic(compressed, compressedSize)) {
         error("missing SOI marker, aborting!");
+
+        free(compressed);
+        if (metaBuf != NULL)
+            free(metaBuf);
+
         return 1;
     }
 
     /* Make sure APP0 is recorded immediately after the SOI marker. */
     if (compressed[2] != 0xff || compressed[3] != 0xe0) {
         error("missing APP0 marker, aborting!");
+
+        free(compressed);
+        if (metaBuf != NULL)
+            free(metaBuf);
+
+        return 1;
+    }
+
+    // Open output file for writing
+    file = openOutput(outputPath);
+    if (file == NULL) {
+        error("could not open output file: %s", outputPath);
+
+        free(compressed);
+        if (metaBuf != NULL)
+            free(metaBuf);
+
         return 1;
     }
 
     /* Write SOI marker and APP0 metadata to the output file. */
-    int app0_len = (compressed[4] << 8) + compressed[5];
-    fwrite(compressed, 4 + app0_len, 1, file);
+    unsigned int app0_lenP4 = 4 + (compressed[4] << 8) + compressed[5];
+    int wSize = fwrite(compressed, app0_lenP4, 1, file);
+    if (wSize != 1) {
+        fclose(file);
+        error("could not write to output file: %s", outputPath);
+
+        free(compressed);
+        if (metaBuf != NULL)
+            free(metaBuf);
+
+        return 1;
+    }
 
     /*
      * Write comment (COM metadata) so we know not to reprocess this file in
      * the future if it gets passed in again.
      */
+    int comLen = strlen(COMMENT);
     fputc(0xff, file);
     fputc(0xfe, file);
     fputc(0x00, file);
-    fputc(strlen(COMMENT) + 2, file);
-    fwrite(COMMENT, strlen(COMMENT), 1, file);
+    fputc(comLen + 2, file);
+    wSize = fwrite(COMMENT, comLen, 1, file);
+    if (wSize != 1) {
+        fclose(file);
+        error("could not write to output file: %s", outputPath);
+
+        free(compressed);
+        if (metaBuf != NULL)
+            free(metaBuf);
+
+        return 1;
+    }
 
     /* Write additional metadata markers. */
-    if (inputFiletype == FILETYPE_JPEG && !strip) {
-        fwrite(metaBuf, metaSize, 1, file);
+    if (metaBuf != NULL) {
+        wSize = fwrite(metaBuf, metaSize, 1, file);
+
+        free(metaBuf);
+
+        if (wSize != 1) {
+            fclose(file);
+            error("could not write to output file: %s", outputPath);
+
+            free(compressed);
+
+            return 1;
+        }
     }
 
     /* Write image data. */
-    fwrite(compressed + 4 + app0_len, compressedSize - 4 - app0_len, 1, file);
-    fclose(file);
-
-    if (inputFiletype == FILETYPE_JPEG && !strip) {
-        free(metaBuf);
-    }
+    wSize = fwrite(compressed + app0_lenP4, compressedSize - app0_lenP4, 1, file);
 
     free(compressed);
-    free(original);
-    free(originalGray);
+
+    if (wSize != 1) {
+        fclose(file);
+        error("could not write to output file: %s", outputPath);
+
+        return 1;
+    }
+
+    int err = fclose(file);
+    if (err) {
+        error("could not close the output file: %s", outputPath);
+        return 1;
+    }
 
     return 0;
 }
